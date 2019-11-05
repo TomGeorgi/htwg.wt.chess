49caa700ed163df75bddbac3d165fd10b41b1259
Tom Georgi <tom.georgi@htwg-konstanz.de>
2019-11-05 22:44:35 +0100

    Updated Chess Game. Added bootstrap. Chess Site and About site are responsive now.

--- app/controllers/ChessController.scala
+++ app/controllers/ChessController.scala
@@ -9,34 +9,69 @@ import de.htwg.se.Chess.controller.controllerComponent.GameStatus
 @Singleton
 class ChessController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val gameController = Chess.controller
+  def message = GameStatus.message(gameController.gameStatus)
   def chessAsText =  gameController.gridToString + GameStatus.message(gameController.gameStatus)
 
-  def about= Action {
+  var pointOneRow = -1
+  var pointOneCol = -1
+  var pointTwoRow = -1
+  var pointTwoCol = -1
+
+  def about = Action {
     Ok(views.html.index())
   }
 
   def chess = Action {
-    Ok(views.html.chess(gameController))
+    print(gameController.gridToString)
+    Ok(views.html.chess(gameController, message))
 //    Ok(chessAsText)
   }
 
-  def set(row:Int, col:Int, row2:Int,col2:Int) = Action {
+  def turn(row:Int, col:Int, row2:Int,col2:Int) = Action {
     gameController.turn(row, col, row2, col2)
-    Ok(views.html.chess(gameController))
+    Ok(views.html.chess(gameController, message))
+  }
+
+  def turn_intern(row:Int, col:Int, row2:Int,col2:Int) {
+    gameController.turn(row, col, row2, col2)
   }
 
   def undo = Action {
     gameController.undo
-    Ok(views.html.chess(gameController))
+    Ok(views.html.chess(gameController, message))
   }
 
   def redo = Action {
     gameController.redo
-    Ok(views.html.chess(gameController))
+    Ok(views.html.chess(gameController, message))
   }
 
   def new_game(playerOne: String, playerTwo: String) = Action {
     gameController.createNewGrid(playerOne, playerTwo)
-    Ok(views.html.chess(gameController))
+    pointOneRow = -1
+    pointOneCol = -1
+    pointTwoRow = -1
+    pointTwoCol = -1
+    Ok(views.html.chess(gameController, message))
+  }
+
+  def point(row: Int, col: Int) = Action {
+    if (pointOneRow == -1 && pointOneCol == -1) {
+      pointOneRow = row
+      pointOneCol = col
+    } else if (pointTwoRow == -1 && pointTwoCol == -1) {
+      pointTwoRow = row
+      pointTwoCol = col
+    }
+
+    if (pointOneRow != -1 && pointOneCol != -1 && pointTwoRow != -1 && pointTwoCol != -1) {
+      turn_intern(pointOneRow, pointOneCol, pointTwoRow, pointTwoCol)
+      pointOneRow = -1
+      pointOneCol = -1
+      pointTwoRow = -1
+      pointTwoCol = -1
+    }
+
+    Ok(views.html.chess(gameController, message))
   }
 }
